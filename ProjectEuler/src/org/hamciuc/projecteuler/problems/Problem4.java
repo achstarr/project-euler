cad1cd64a718d6d71d1bbdeb75e41bb509010253
Adrian Hamciuc <adrian.hamciuc@gmail.com>
2012-06-07 22:21:49 +0300

    move method to utils class

--- ProjectEuler/src/org/hamciuc/projecteuler/problems/Problem4.java
+++ ProjectEuler/src/org/hamciuc/projecteuler/problems/Problem4.java
@@ -1,24 +1,29 @@
 package org.hamciuc.projecteuler.problems;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.hamciuc.projecteuler.utils.ProblemUtils;
+
+/**
+ * A palindromic number reads the same both ways. The largest palindrome made
+ * from the product of two 2-digit numbers is 9009 = 91 × 99.
+ * 
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ * 
+ * http://projecteuler.net/problem=4
+ * 
+ * @author Adrian
+ * 
+ */
 public class Problem4 {
-	private static boolean isPalindrome(int x) {
-		String xAsString = Integer.toString(x);
-		StringBuffer sb = new StringBuffer(xAsString);
-		return sb.reverse().toString().equals(xAsString);
 
-	}
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		List<Integer> palindromes = new ArrayList<Integer>();
 		for (int i = 100; i < 1000; i++) {
 			for (int j = 100; j < 1000; j++) {
-				if (isPalindrome(i * j)) {
+				if (ProblemUtils.isPalindrome(i * j)) {
 					palindromes.add(i * j);
 				}
 			}
---
+++ ProjectEuler/src/org/hamciuc/projecteuler/utils/ProblemUtils.java
@@ -0,0 +1,23 @@
+package org.hamciuc.projecteuler.utils;
+
+/**
+ * Contains various utility methods called in several problems
+ * 
+ * @author Adrian
+ * 
+ */
+public class ProblemUtils {
+	/**
+	 * Returns true if the number to be verified is a palindrome, false
+	 * otherwise. TODO: optimize
+	 * 
+	 * @param x
+	 * @return
+	 */
+	public static boolean isPalindrome(int x) {
+		String xAsString = Integer.toString(x);
+		StringBuffer sb = new StringBuffer(xAsString);
+		return sb.reverse().toString().equals(xAsString);
+
+	}
+}
