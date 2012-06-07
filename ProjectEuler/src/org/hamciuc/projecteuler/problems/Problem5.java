4074f18cba974323149267bd8a77d2715ad1510e
Adrian Hamciuc <adrian.hamciuc@gmail.com>
2012-06-07 22:23:10 +0300

    add description

--- ProjectEuler/src/org/hamciuc/projecteuler/problems/Problem5.java
+++ ProjectEuler/src/org/hamciuc/projecteuler/problems/Problem5.java
@@ -1,12 +1,19 @@
 package org.hamciuc.projecteuler.problems;
-import java.util.ArrayList;
-import java.util.List;
 
+/**
+ * 2520 is the smallest number that can be divided by each of the numbers from 1
+ * to 10 without any remainder.
+ * 
+ * What is the smallest positive number that is evenly divisible by all of the
+ * numbers from 1 to 20?
+ * 
+ * http://projecteuler.net/problem=5
+ * 
+ * @author Adrian
+ * 
+ */
 public class Problem5 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		int result = 2520 * 11 * 13 * 2 * 17 * 19;
 		System.out.println(result);
