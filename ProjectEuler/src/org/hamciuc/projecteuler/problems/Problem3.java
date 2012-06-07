1b0f20176009805bb7c258de6dc29a8368328a00
Adrian Hamciuc <adrian.hamciuc@gmail.com>
2012-06-07 22:17:46 +0300

    compact code, remove unneeded method

--- ProjectEuler/src/org/hamciuc/projecteuler/problems/Problem3.java
+++ ProjectEuler/src/org/hamciuc/projecteuler/problems/Problem3.java
@@ -1,22 +1,28 @@
 package org.hamciuc.projecteuler.problems;
+
+/**
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * 
+ * What is the largest prime factor of the number 600851475143 ?
+ * 
+ * http://projecteuler.net/problem=3
+ * 
+ * @author Adrian
+ * 
+ */
 public class Problem3 {
 
-	private static long nextPrime(long currentPrime) {
-		return currentPrime + 1;
-	}
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		long number = 600851475143l;
 		long currentPrime = 2;
 		while (number != 1) {
-			if (number % currentPrime == 0) {
+			if (number % currentPrime++ == 0) {
 				number /= currentPrime;
 			}
-			currentPrime = nextPrime(currentPrime);
+			currentPrime++;
 		}
+		// compensate for the extra increment after the result was found
+		currentPrime--;
 		System.out.println("Result is " + currentPrime);
 	}
 }
