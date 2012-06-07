342a8a13ec926e4a8d25e3e5373a6f3fcaa3a907
Adrian Hamciuc <adrian.hamciuc@gmail.com>
2012-06-07 22:31:14 +0300

    added description + link to problem

--- ProjectEuler/src/org/hamciuc/projecteuler/problems/Problem6.java
+++ ProjectEuler/src/org/hamciuc/projecteuler/problems/Problem6.java
@@ -1,15 +1,30 @@
 package org.hamciuc.projecteuler.problems;
 
+/**
+ * The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... +
+ * 10^2 = 385
+ * 
+ * The square of the sum of the first ten natural numbers is, (1 + 2 + ... +
+ * 10)^2 = 55^2 = 3025
+ * 
+ * Hence the difference between the sum of the squares of the first ten natural
+ * numbers and the square of the sum is 3025 − 385 = 2640.
+ * 
+ * Find the difference between the sum of the squares of the first one hundred
+ * natural numbers and the square of the sum.
+ * 
+ * http://projecteuler.net/problem=6
+ * 
+ * @author Adrian
+ * 
+ */
 public class Problem6 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		int sumOfSquares = 0;
-		int squareOfSum = 50*101*50*101;
-		for(int i=1;i<=100;i++){
-			sumOfSquares +=i*i;
+		int squareOfSum = 50 * 101 * 50 * 101;
+		for (int i = 1; i <= 100; i++) {
+			sumOfSquares += i * i;
 		}
 		System.out.println("Result is " + Math.abs(sumOfSquares - squareOfSum));
 
